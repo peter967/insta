6cb439b59cf95fbfc527095e9a15eaf0dd5a0195
peter <84807256+peter967@users.noreply.github.com>
2023-04-29 04:47:23 +0300

    google Auth

--- lib/state/auth/backend/authenticator.dart
+++ lib/state/auth/backend/authenticator.dart
@@ -1,4 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:insta/state/auth/constants/constants.dart';
+import 'package:insta/state/auth/models/auth_result.dart';
 import 'package:insta/state/posts/typedes/user_id.dart';
 
 class Authenticator {
@@ -6,4 +9,34 @@ class Authenticator {
   UserId? get userId => currentUser?.uid;
   bool get isAlreadyLoggedIn => userId != null;
   String get displayNAme => currentUser?.displayName ?? '';
+  String? get email => currentUser?.email;
+  Future<void> logOut() async {
+    await FirebaseAuth.instance.signOut();
+    await GoogleSignIn().signOut();
+  }
+
+  Future<AuthResult> loginWithGoogle() async {
+    final GoogleSignIn googleSignIn = GoogleSignIn(
+      scopes: [
+        Constants.emailScope,
+      ],
+    );
+    final signInAccount = await googleSignIn.signIn();
+    if (signInAccount == null) {
+      return AuthResult.aborted;
+    }
+    final googleAuth = await signInAccount.authentication;
+    final oAuthCredentials = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    try {
+      await FirebaseAuth.instance.signInWithCredential(
+        oAuthCredentials,
+      );
+      return AuthResult.success;
+    } catch (e) {
+      return AuthResult.failure;
+    }
+  }
 }
