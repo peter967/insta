1f51e750febb3e0ed07d446045edff4305eb1b21
peter <84807256+peter967@users.noreply.github.com>
2023-05-03 10:27:23 +0300

    lottie animation view

---
+++ lib/views/components/animations/lottie_animation_view.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:lottie/lottie.dart';
+
+import 'models/lottie_animation.dart';
+
+class LottieAnimationView extends StatelessWidget {
+  const LottieAnimationView({
+    Key? key,
+    required this.animation,
+    this.repeat = true,
+    this.revers = false,
+  }) : super(key: key);
+  final LottieAnimation animation;
+  final bool repeat;
+  final bool revers;
+  @override
+  Widget build(BuildContext context) => Lottie.asset(
+        animation.fullPath,
+        repeat: repeat,
+        reverse: revers,
+      );
+}
+
+extension GetFullPath on LottieAnimation {
+  String get fullPath => 'assets/animation/$name.json';
+}
